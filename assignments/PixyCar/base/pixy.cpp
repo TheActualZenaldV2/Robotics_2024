f10b5d2562d8a0fe6c210ddf77e14cef3d6efa8b
Jonathan Luciu <115652432+TheActualZenaldV2@users.noreply.github.com>
2024-04-19 14:34:15 -0700

    Create pixy.cpp

---
+++ assignments/PixyCar/base/pixy.cpp
@@ -0,0 +1,103 @@
+/*
+
+// b-pixy2-SPI.ino
+
+-------------------------------------------------------------------------
+
+Best to use the Portenta Pro Community solutions arduino library
+and load the menu accessoies--> sensors --> dot212-pixy2-SPI    sketch.
+The above has all of the several include files needed by the Pixy2 camera
+---------------------------------------------------------------------------
+
+Pixy library easy install zipped file at 
+https://github.com/charmedlabs/pixy2/raw/master/releases/arduino/arduino_pixy2-1.0.3.zip
+
+weird extra where you have to delete the last 4 zumo files in he library 
+checkout the last part of these installation documents
+https://docs.pixycam.com/wiki/doku.php?id=wiki:v2:hooking_up_pixy_to_a_microcontroller_-28like_an_arduino-29
+
+Read about it here 
+https://pixycam.com/pixy2
+
+
+// By Jeremy Ellis
+// MIT License
+
+
+
+
+
+// SPI pins on the photon
+// See connection images on github at 
+//   https://github.com/hpssjellis/particle.io-photon-high-school-robotics/tree/master/a15-serial-SPI
+
+
+// VIN   = Portenta +3V3
+// GND   = Portenta GND
+// MISO  = Portenta D10 (PC_2)
+// SCK   = Portenta D9  (PI_1)
+// MOSI  = Portenta D8  (PC_3)
+
+// 6 prong cable using the red wire at top to the left
+//  3V3, D10
+//   D8, D9
+//  GND, NC    (not connected)
+
+
+
+
+if using platform.io you need this library in the platformio.ini file
+
+lib_deps = arduino12/pixy2@^1.0.4
+
+
+*/
+
+
+#include <Arduino.h> // Only needed by https://platformio.org/
+
+int wow = 2;   // just seems to need an integer
+
+#include "Pixy2.h"
+#include "TPixy2.h"
+
+Pixy2 pixy;                          // Create our pixy object
+
+// Setup - Runs Once @ Startup
+void setup() {   
+
+   Serial.begin(115200);
+   pixy.init();                    // Initalize the pixy object
+}
+
+// Loop - Runs over and over
+void loop(){ 
+
+   // uint16_t    blocks;                       // Create an unsigned int to hold the number of found blocks
+    char        buf[50];                      // Create a buffer for printing over serial
+    int         myPixyInt, myPixyIntY, myPixyIntWidth;
+
+    pixy.ccc.getBlocks();     // Do the pixy stuff. Grab the numbers of blocks the pixy finds
+
+  // If we have some blocks increment the frame counter (i) and if enough frames have passed print out data about the found blocks to the serial port
+  if (pixy.ccc.numBlocks) {
+
+    myPixyInt = pixy.ccc.blocks[0].m_x;    // x location of the main object    0 - 320
+    myPixyIntY = pixy.ccc.blocks[0].m_y;    // y location of the main object    0 - 320
+    myPixyIntWidth = pixy.ccc.blocks[0].m_width;    // width of the main object    0 - 320
+
+    Serial.print("X Location: ");
+    Serial.println(myPixyInt);  
+
+    Serial.print("Y Location: ");
+    Serial.println(myPixyIntY);  
+
+    Serial.print("Width: ");
+    Serial.println(myPixyIntWidth);  
+
+  }  else {  // no blocks seen by camera
+    Serial.println("Pixy No Object");       
+  }
+           
+   delay(100); // just for the heck of it slow things down a bit
+}
